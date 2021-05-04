68b04d53612351fb71dd7dc1b7d669896ce00589
Elton Bakia <elton.bakia@marconi.loc>
2021-05-04 15:22:02 +0200

    .

---
+++ components/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div style="background-image: url('assets/img/logo.png');">
+  <h1>dafjaefjef </h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- components/navbar.vue
+++ components/navbar.vue
@@ -7,122 +7,63 @@
 		class="flex flex-col items-center bg-white text-gray-700 shadow h-full">
 		<!-- Side Nav Bar-->
 
-		<div class="h-16 flex items-center w-full">
+		<div class="h-16 flex items-center w-full mt-8">
 			<!-- Logo Section -->
 			<a class="h-15 w-15 mx-auto" href="#">
 				<img
 					class="h-12 w-12 mx-auto"
-					src="~/assets/logo.png"
+					src="~/assets/img/logo.png"
 					alt="logo" />
 			</a>
 		</div>
 
 		<ul>
 			<!-- Items Section -->
-			<li class="hover:bg-gray-100">
+			<li class="hover:bg-gray-100 mt-8">
 				<a
 					href="."
 					class="h-16 px-6 flex flex justify-center items-center w-full
 					focus:text-orange-500">
-					<svg
-						class="h-5 w-5"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round">
-						<polyline
-							points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
-						<path
-							d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
-							2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0
-							0-1.79 1.11z"></path>
-					</svg>
+					<img class="h-10 w-12" src="~/assets/img/icona_home.png" alt="">
 
 				</a>
 			</li>
 
-			<li class="hover:bg-gray-100">
+			<li class="hover:bg-gray-100 mt-8">
 				<a
 					href="."
 					class="h-16 px-6 flex flex justify-center items-center w-full
 					focus:text-orange-500">
-					<svg
-						class="h-5 w-5"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round">
-						<path
-							d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-					</svg>
+					<img class="h-10 w-12" src="~/assets/img/icona_cuffie.png" alt="">
+                    
 
 				</a>
 			</li>
 
-			<li class="hover:bg-gray-100">
+			<li class="hover:bg-gray-100 mt-8">
 				<a
 					href="."
 					class="h-16 px-6 flex flex justify-center items-center w-full
 					focus:text-orange-500">
 
-					<svg
-						class="h-5 w-5"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round">
-						<polyline points="3 6 5 6 21 6"></polyline>
-						<path
-							d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2
-							0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-					</svg>
+					<img class="h-12 w-12" src="~/assets/img/icona_news.png" alt="">
 
 				</a>
 			</li>
 
-			<li class="hover:bg-gray-100">
+			<li class="hover:bg-gray-100 mt-8">
 				<a
 					href="."
 					class="h-16 px-6 flex flex justify-center items-center w-full
 					focus:text-orange-500">
-					<svg
-						class="h-5 w-5"
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round">
-						<circle cx="9" cy="21" r="1"></circle>
-						<circle cx="20" cy="21" r="1"></circle>
-						<path
-							d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0
-							2-1.61L23 6H6"></path>
-					</svg>
+					<img class="h-12 w-12" src="~/assets/img/icona_punto_interrogativo.png" alt="">
 
 				</a>
 			</li>   
 
 		</ul>
 	</aside>
+    <home></home>
 </div>
 
 </template>
