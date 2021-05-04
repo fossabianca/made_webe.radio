8ec220e2cfe888eb0fa2da64c720f157773fce86
Elton Bakia <elton.bakia@marconi.loc>
2021-05-04 13:57:01 +0200

    int

---
+++ components/navbar.vue
@@ -0,0 +1,131 @@
+<template>
+
+<div class="h-screen w-screen flex bg-gray-200">
+	<!-- container -->
+
+	<aside
+		class="flex flex-col items-center bg-white text-gray-700 shadow h-full">
+		<!-- Side Nav Bar-->
+
+		<div class="h-16 flex items-center w-full">
+			<!-- Logo Section -->
+			<a class="h-15 w-15 mx-auto" href="#">
+				<img
+					class="h-12 w-12 mx-auto"
+					src="~/assets/logo.png"
+					alt="logo" />
+			</a>
+		</div>
+
+		<ul>
+			<!-- Items Section -->
+			<li class="hover:bg-gray-100">
+				<a
+					href="."
+					class="h-16 px-6 flex flex justify-center items-center w-full
+					focus:text-orange-500">
+					<svg
+						class="h-5 w-5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<polyline
+							points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
+						<path
+							d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
+							2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0
+							0-1.79 1.11z"></path>
+					</svg>
+
+				</a>
+			</li>
+
+			<li class="hover:bg-gray-100">
+				<a
+					href="."
+					class="h-16 px-6 flex flex justify-center items-center w-full
+					focus:text-orange-500">
+					<svg
+						class="h-5 w-5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<path
+							d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+					</svg>
+
+				</a>
+			</li>
+
+			<li class="hover:bg-gray-100">
+				<a
+					href="."
+					class="h-16 px-6 flex flex justify-center items-center w-full
+					focus:text-orange-500">
+
+					<svg
+						class="h-5 w-5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<polyline points="3 6 5 6 21 6"></polyline>
+						<path
+							d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2
+							0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+					</svg>
+
+				</a>
+			</li>
+
+			<li class="hover:bg-gray-100">
+				<a
+					href="."
+					class="h-16 px-6 flex flex justify-center items-center w-full
+					focus:text-orange-500">
+					<svg
+						class="h-5 w-5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<circle cx="9" cy="21" r="1"></circle>
+						<circle cx="20" cy="21" r="1"></circle>
+						<path
+							d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0
+							2-1.61L23 6H6"></path>
+					</svg>
+
+				</a>
+			</li>   
+
+		</ul>
+	</aside>
+</div>
+
+</template>
+<script>
+export default {}
+</script>
