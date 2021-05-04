477c037bcb4ce822dd4033531765ec4c08ce2b2b
fossabianca <57177524+fossabianca@users.noreply.github.com>
2021-05-04 20:02:32 +0200

    ,

---
+++ components/about.vue
@@ -0,0 +1,46 @@
+<template>
+<h1 id="progetto-webe-radio">Progetto Webe-radio</h1>
+<h2 id="sintesi">Sintesi</h2>
+<p>Il progetto Webe radio mira alla realizzazione di una web radio, costituita e gestita da una rete di scuole distribuite sul territorio provinciale, e precisamente l’Istituto Tecnico Tecnologico “Marconi” di Rovereto (capofila), Istituto Tecnico Economico &quot;A. Tambosi&quot; di Trento e Istituto di Istruzione &quot;La Rosa Bianca&quot; di Cavalese/Predazzo.</p>
+<p>Ogni istituto costituisce una redazione autonoma con dispositivi propri di trasmissione coordinati da una regia in un unico palinsesto. Nasce come forma di didattica sperimentale e innovativa tesa al coinvolgimento diretto e motivazionale dei giovani: uno strumento poliedrico e multidisciplinare che permette di allenare competenze sia tecniche che comunicative.</p>
+<h2 id="obiettivi">Obiettivi</h2>
+<ul>
+<li>Coinvolgere giovani in azioni di socializzazione, informazione e comunicazione utilizzando le tecnologie digitali per favorire l’integrazione, contrastare fenomeni di discriminazione, di isolamento e di intolleranza.</li>
+<li>Orientare i giovani verso la cultura tecnologica in modo sano contribuendo a sviluppare quella capacità critica necessaria per esercitare una cittadinanza attiva.</li>
+<li>Favorire lo sviluppo del dialogo e progettazione tra Istituti e docenti che condividono la necessità della sperimentazione di nuove modalità ed azioni per la didattica</li>
+</ul>
+<h2 id="cronistoria-del-progetto">Cronistoria del progetto</h2>
+<ul>
+<li><strong>Anno scolastico 2019/20</strong>: l’IPRASE promuove un percorso di formazione per i docenti dal titolo “Dall’Alternanza Scuola Lavoro alla creazione di una start-up: come nasce una radio web” e che durante gli incontri è stata presentata, in particolare, l’esperienza del Liceo “Kennedy” di Roma, dove da anni, in collaborazione con una rete di scuole della Capitale, è attiva una web radio scolastica in cui sono protagonisti gli stessi studenti;</li>
+<li><strong>Settembre – Dicembre 2020</strong>: studio e progettazione della realizzazione di una web radio promossa da una rete scolastica diffusa sul territorio provinciale. Matura la convinzione che il progetto può rappresentare una opportunità importante per sviluppare competenze tecniche, culturali e di cittadinanza. La web radio scolastica può diventare un valido strumento di innovazione didattica: innanzi tutto permette il coinvolgimento di studenti e docenti di Istituzioni Scolastiche differenti, favorendo la collaborazione e lo sviluppo di relazioni tra le diverse realtà dello stesso territorio; in secondo luogo, è un tipo di esperienza che si può attivare anche in tempi segnati dall’emergenza epidemiologica, rientrando a pieno titolo all’interno delle attività di alternanza scuola-lavoro, estremamente sentita e apprezzata dai giovani.</li>
+<li><strong>Novembre 2020</strong>: l’I. T. T. “G. Marconi” di Rovereto, in qualità di capofila degli istituti I. T. E. “A. Tambosi” di Trento e I. I. La Rosa Bianca di Cavalese, partecipa al bando “Comunità scolastiche che ripartono” della FONDAZIONE CARITRO con il progetto “WeBeRadio”. Il progetto è ritenuto valido dall’apposita mmissione CARITRO che ne sostiene economicamente l’iniziativa;</li>
+<li><strong>Gennaio 2021</strong>: sottoscritto e firmato l’accordo di rete tra i tre istituti scolastici per la creazione di un’esperienza di web radio scolastica diffusa sul territorio, che fissa finalità e organizzazione e prevede tra l’altro la possibilità di ampliare la rete ad altri istituti o enti. Avvio di una collaborazione con IPRASE (patrocinio dell’IPRASE) e stretta collaborazione fin dall’ideazione del progetto con il Dipartimento Istruzione e Cultura della Provincia.</li>
+<li><strong>30 marzo 2021</strong>: KICKOFF del progetto Webe radio</li>
+</ul>
+<h2 id="attivit-previste">Attività previste</h2>
+<ul>
+<li><h3 id="fase-di-formazione-sperimentazione-a-s-2020-21-">Fase di formazione/sperimentazione (a.s. 2020/21)</h3>
+<ul>
+<li>formazione studenti e docenti</li>
+<li>produzione di podcast</li>
+<li>progettazione e attivazione portale dedicato</li>
+<li>attività di speaking</li>
+</ul>
+</li>
+<li><h3 id="fase-di-consolidamento-e-diffusione-a-s-2021-22-">Fase di consolidamento e diffusione (a.s 2021/22)</h3>
+<ul>
+<li>produzione di trasmissione live</li>
+<li>produzione di podcast</li>
+<li>consolidamento portale dedicato</li>
+<li>creazione eventuale app su smartphone</li>
+</ul>
+</li>
+</ul>
+</template>
+
+<script>
+export default {}
+</script>
+<style>
+
+</style>
--- components/home.vue
+++ components/home.vue
@@ -1,7 +1,13 @@
 <template>
-  <div style="background-image: url('assets/img/logo.png');">
-  <h1>dafjaefjef </h1>
-  </div>
+  <div id="home" class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image:url('https://raw.githubusercontent.com/tailwindtoolbox/Ghostwind/master/cover.jpg'); height: 60vh; max-height:460px;">
+			<div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+				<!--Title-->
+					<p class="text-white font-extrabold text-3xl md:text-5xl">
+              WeBe Radio
+					</p>
+					<p class="text-xl md:text-2xl text-gray-500">Benvenuti su WeBe Radio</p>
+			</div>
+		</div>
 </template>
 
 <script>
