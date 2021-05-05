57c0647ecac4039566f649a813aee39dffc69d82
fossabianca <57177524+fossabianca@users.noreply.github.com>
2021-05-05 10:07:19 +0200

    .

--- components/about.vue
+++ components/about.vue
@@ -1,15 +1,18 @@
 <template>
 <div id="about" class="container max-w-7xl mx-auto break-normal">
 <h1 id="progetto-webe-radio" class="font-black uppercase">Progetto Webe-radio</h1>
-<h2 id="sintesi">Sintesi</h2>
+<br>
+<h2 id="sintesi" class="font-black">Sintesi</h2>
 <p>Il progetto Webe radio mira alla realizzazione di una web radio, costituita e gestita da una rete di scuole distribuite sul territorio provinciale, e precisamente l’Istituto Tecnico Tecnologico “Marconi” di Rovereto (capofila), Istituto Tecnico Economico &quot;A. Tambosi&quot; di Trento e Istituto di Istruzione &quot;La Rosa Bianca&quot; di Cavalese/Predazzo.</p>
 <p>Ogni istituto costituisce una redazione autonoma con dispositivi propri di trasmissione coordinati da una regia in un unico palinsesto. Nasce come forma di didattica sperimentale e innovativa tesa al coinvolgimento diretto e motivazionale dei giovani: uno strumento poliedrico e multidisciplinare che permette di allenare competenze sia tecniche che comunicative.</p>
+<br>
 <h2 id="obiettivi" class="font-black uppercase">Obiettivi</h2>
 <ul class="list-disc">
 <li>Coinvolgere giovani in azioni di socializzazione, informazione e comunicazione utilizzando le tecnologie digitali per favorire l’integrazione, contrastare fenomeni di discriminazione, di isolamento e di intolleranza.</li>
 <li>Orientare i giovani verso la cultura tecnologica in modo sano contribuendo a sviluppare quella capacità critica necessaria per esercitare una cittadinanza attiva.</li>
 <li>Favorire lo sviluppo del dialogo e progettazione tra Istituti e docenti che condividono la necessità della sperimentazione di nuove modalità ed azioni per la didattica</li>
 </ul>
+<br>
 <h2 id="cronistoria-del-progetto" class="font-black uppercase">Cronistoria del progetto</h2>
 <ul class="list-disc">
 <li><p class="font-bold">Anno scolastico 2019/20:</p> l’IPRASE promuove un percorso di formazione per i docenti dal titolo “Dall’Alternanza Scuola Lavoro alla creazione di una start-up: come nasce una radio web” e che durante gli incontri è stata presentata, in particolare, l’esperienza del Liceo “Kennedy” di Roma, dove da anni, in collaborazione con una rete di scuole della Capitale, è attiva una web radio scolastica in cui sono protagonisti gli stessi studenti;</li>
@@ -18,6 +21,7 @@
 <li><p class="font-bold">Gennaio 2021:</p> sottoscritto e firmato l’accordo di rete tra i tre istituti scolastici per la creazione di un’esperienza di web radio scolastica diffusa sul territorio, che fissa finalità e organizzazione e prevede tra l’altro la possibilità di ampliare la rete ad altri istituti o enti. Avvio di una collaborazione con IPRASE (patrocinio dell’IPRASE) e stretta collaborazione fin dall’ideazione del progetto con il Dipartimento Istruzione e Cultura della Provincia.</li>
 <li><p class="font-bold">30 marzo 2021:</p> KICKOFF del progetto Webe radio</li>
 </ul>
+<br>
 <h2 id="attività-previste" class="font-black uppercase">Attività previste</h2>
 <ul class="list-disc">
 <li><h3>Fase di formazione/sperimentazione (a.s. 2020/21)</h3>
@@ -27,7 +31,9 @@
 <li>progettazione e attivazione portale dedicato</li>
 <li>attività di speaking</li>
 </ul></li>
-<li><h3>Fase di consolidamento e diffusione (a.s 2021/22)</h3>
+<br>
+<li>
+<h3>Fase di consolidamento e diffusione (a.s 2021/22)</h3>
 <ul class="list-decimal">
 <li>produzione di trasmissione live</li>
 <li>produzione di podcast</li>
