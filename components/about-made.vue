0efaa8adc6fa8f287b4e4b05fb903584cbdd9a81
fossabianca <francesco.bonomi03@gmail.com>
2021-05-07 14:26:27 +0200

    progetto made about +

---
+++ components/about-made.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="about" class="container max-w-7xl mx-auto break-normal p-20">
+<h1 class="font-black uppercase">progetto made</h1>
+<br>
+<p>Il Progetto, già sperimentato nei precedenti quattro anni scolastici, prevede la collaborazione tra il nostro Istituto e il Liceo Artistico “F.Depero” di Rovereto con l’obiettivo finale di realizzare siti web coniugando le diverse competenze che gli indirizzi presenti permettono agli studenti di acquisire. 
+Nello specifico verranno coinvolti gli studenti dell’indirizzo di Grafica del Liceo Artistico “F. Depero” che si occuperanno prevalentemente dell’interfaccia grafica in tutti gli aspetti di loro competenza, mentre gli studenti dell’indirizzo di Informatica del nostro Istituto si occuperanno della realizzazione della struttura del sito web e della gestione dell&#39;archivio delle informazioni.
+Una delle ipotesi possibili prevede un lavoro su commessa dl un Museo di Rovereto.</p>
+<br>
+<p>Il Progetto ha i seguenti obiettivi:
+<ul class="list-disc">
+<li>incrociare competenze acquisite in percorsi formativi diversi, apparentemente distanti</li>
+<li>sperimentare la modalità di workteam, che è una delle skill maggiormente richieste dal mondo del lavoro</li>
+<li>sperimentare una modalità di interazione professionale che è tipica nella realizzazione di prodotti legati al web</li>
+<li>promuovere una modalità innovativa di collaborazione, che favorisca la contaminazione “virtuosa” tra istituti diversi presenti sul territorio.</li>
+</ul>
+</p>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
     <podcast></podcast>
     <news></news>
     <about></about>
+    <about-made></about-made>
+    
   </div>
 </template>
 <style>
@@ -14,10 +16,11 @@ html {
 </style>
 
 <script>
+import AboutMade from '../components/about-made.vue';
 import About from "../components/about.vue";
 import home from "../components/home.vue";
 import Podcast from "../components/podcast.vue";
 export default {
-  components: { home, About, Podcast }
+  components: { home, About, Podcast, AboutMade}
 };
 </script>
